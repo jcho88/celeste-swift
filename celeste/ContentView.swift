e3d39028c23a1c20167778486af2aeb6def651b5
Justin Cho <justinmyungcho@gmail.com>
2022-02-21 12:51:19 -0800

    new swift ui project

---
+++ celeste/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  celeste
+//
+//  Created by Justin Cho on 2/21/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ celeste/celesteApp.swift
@@ -0,0 +1,17 @@
+//
+//  celesteApp.swift
+//  celeste
+//
+//  Created by Justin Cho on 2/21/22.
+//
+
+import SwiftUI
+
+@main
+struct celesteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
